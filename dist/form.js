f72c511a4940a4cf8d315627ce40a1359e87beeb
Mohammad <hasaeen3252@gmail.com>
2022-08-10 11:12:00 +0300

    editing main

---
+++ dist/form.js
@@ -0,0 +1,560 @@
+//education
+$('.add-education').on('click', function () {
+  let educationID = $('.educations')
+    .find('.education-item:last-child')
+    .data('id');
+  if (!educationID) {
+    educationID = 0;
+  }
+  educationID++;
+  $(this).closest('div').siblings('.accordion-body').append(`
+    <div class="education-item mb-3" data-id=${educationID}>
+      <form class=row>
+        <div class="mb-1 col-12">
+          <label for=education-${educationID} class=form-label>Education</label>
+          <input type=text class=form-control id=education-${educationID} />
+        </div>
+        <div class="mb-1 col-6">
+         <label for=educationUniversity-${educationID} class=form-label
+           >University</label
+         >
+         <input
+           type=text
+           class=form-control
+           id=educationUniversity-${educationID}
+         />
+        </div>
+        <div class="mb-1 col-6">
+          <label for=educationUniversityAddress-${educationID} class=form-label
+            >Address</label
+          >
+          <input
+            type=text
+            class=form-control
+            id=educationUniversityAddress-${educationID}
+          />
+        </div>
+        <div class="mb-1 col-6">
+          <label for=educationStartDate-${educationID} class=form-label
+            >Start Date</label
+          >
+          <input
+            type=date
+            class=form-control
+            id=educationStartDate-${educationID}
+          />
+        </div>
+        <div class="mb-1 col-6">
+          <label for=educationEndDate-${educationID} class=form-label
+            >End Date</label
+          >
+          <input
+            type=date
+            class=form-control
+            id=educationEndDate-${educationID}
+          />
+        </div>
+        <div class="mb-1 col-12">
+          <label for=educationDescription-${educationID} class=form-label
+            >Description</label
+          >
+          <textarea
+            class=form-control
+            id=educationDescription-${educationID}
+            rows="3"
+          ></textarea>
+        </div>
+      </form>
+      <button
+        type=button
+        class="btn btn-outline-danger remove-education"
+      >
+        <svg
+          xmlns=http://www.w3.org/2000/svg
+          width=16
+          height=16
+          fill=currentColor
+          class="bi bi-trash-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+          ></path>
+        </svg>
+        Remove
+      </button>
+      <button
+        type=button
+        class="btn btn-outline-success send-education"
+      >
+        <svg
+          xmlns=http://www.w3.org/2000/svg
+          width=16
+          height=16
+          fill=currentColor
+          class="bi bi-check2"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
+          ></path>
+        </svg>
+        Done
+      </button>
+    </div>
+  `);
+});
+
+$('.accordion-body').on('click', '.remove-education', function () {
+  $(this).closest('.education-item').remove();
+});
+
+$('.accordion-body').on('click', '.send-education', function () {
+  const educationID = $(this).closest('.education-item').data('id');
+  const education = $(`#education-${educationID}`).val();
+  const educationUniversity = $(`#educationUniversity-${educationID}`).val();
+  const educationUniversityAddress = $(
+    `#educationUniversityAddress-${educationID}`
+  ).val();
+  const educationStartDate = $(`#educationStartDate-${educationID}`).val();
+  const educationEndDate = $(`#educationEndDate-${educationID}`).val();
+  const educationDescription = $(`#educationDescription-${educationID}`).val();
+  console.log(
+    education,
+    educationUniversity,
+    educationUniversityAddress,
+    educationStartDate,
+    educationEndDate,
+    educationDescription
+  );
+});
+
+//work
+$('.add-work').on('click', function () {
+  let workID = $('.works').find('.work-item:last-child').data('id');
+  if (!workID) {
+    workID = 0;
+  }
+  workID++;
+  $(this).closest('div').siblings('.accordion-body').append(`
+    <div class="accordion-body works">
+      <div class="work-item mb-3" data-id=${workID}>
+        <form class=row>
+          <div class="mb-1 col-12">
+            <label for=workPosition-${workID} class=form-label
+              >Position</label
+            >
+            <input type=text class=form-control id=workPosition-${workID} />
+          </div>
+          <div class="mb-1 col-6">
+            <label for=workEmployer-${workID} class=form-label
+              >Employer</label
+            >
+            <input type=text class=form-control id=workEmployer-${workID} />
+          </div>
+          <div class="mb-1 col-6">
+            <label for=workAddress-${workID} class=form-label>Address</label>
+            <input type=text class=form-control id=workAddress-${workID} />
+          </div>
+          <div class="mb-1 col-6">
+            <label for=workStartDate-${workID} class=form-label
+              >Start Date</label
+            >
+            <input
+              type=date
+              class=form-control
+              id=workStartDate-${workID}
+            />
+          </div>
+          <div class="mb-1 col-6">
+            <label for=workEndDate-${workID} class=form-label>End Date</label>
+            <input type=date class=form-control id=workEndDate-${workID} />
+          </div>
+          <div class="mb-1 col-12">
+            <label for=workDescription-${workID} class=form-label
+              >Description</label
+            >
+            <textarea
+              class=form-control
+              id=workDescription-${workID}
+              rows=3
+            ></textarea>
+          </div>
+        </form>
+        <button type=button class="btn btn-outline-danger remove-work">
+          <svg
+            xmlns=http://www.w3.org/2000/svg
+            width=16
+            height=16
+            fill=currentColor
+            class="bi bi-trash-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+            ></path>
+          </svg>
+          Remove
+        </button>
+        <button type=button class="btn btn-outline-success send-work">
+          <svg
+            xmlns=http://www.w3.org/2000/svg
+            width=16
+            height=16
+            fill=currentColor
+            class="bi bi-check2"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
+            ></path>
+          </svg>
+          Done
+        </button>
+      </div>
+    </div>
+  `);
+});
+
+$('.accordion-body').on('click', '.remove-work', function () {
+  $(this).closest('.work-item').remove();
+});
+
+$('.accordion-body').on('click', '.send-work', function () {
+  const workID = $(this).closest('.work-item').data('id');
+  const workPosition = $(`#workPosition-${workID}`).val();
+  const workEmployer = $(`#workEmployer-${workID}`).val();
+  const workAddress = $(`#workAddress-${workID}`).val();
+  const workStartDate = $(`#workStartDate-${workID}`).val();
+  const workEndDate = $(`#workEndDate-${workID}`).val();
+  const workDescription = $(`#workDescription-${workID}`).val();
+  console.log(
+    workPosition,
+    workEmployer,
+    workAddress,
+    workStartDate,
+    workEndDate,
+    workDescription
+  );
+});
+
+//skill
+$('.add-skill').on('click', function () {
+  let skillID = $('.skills').find('.skill-item:last-child').data('id');
+  if (!skillID) {
+    skillID = 0;
+  }
+  skillID++;
+  $(this).closest('div').siblings('.accordion-body').append(`
+    <div class='skill-item mb-2' data-id=${skillID}>
+      <div class="mb-1">
+       <label for=skill-${skillID} class=form-label>Skill</label>
+       <input type="text" class=form-control id=skill-${skillID} />
+       </div>
+       <div class="row">
+         <div class="skill-range col-8">
+           <label for=skill-range-${skillID} class=form-label>Level</label>
+           <input
+             type=range
+             class=form-range
+             min=0
+             value=0
+             max=5
+             step=1
+             id=skill-range-${skillID}
+           />
+         </div>
+         <div class="skill-evaluation col-4 pt-4"></div>
+       </div>
+      <button type=button class="btn btn-outline-danger remove-skill">
+        <svg
+          xmlns=http://www.w3.org/2000/svg
+          width=16
+          height=16
+          fill=currentColor
+          class="bi bi-trash-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+          ></path>
+        </svg>
+        Remove Skill
+      </button>
+      <button type="button" class="btn btn-outline-success send-skill">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-check2"
+          viewBox="0 0 16 16"
+          >
+          <path
+            d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
+          ></path>
+        </svg>
+        Done
+      </button>
+    </div>
+  `);
+});
+
+$('.accordion-body').on('click', '.remove-skill', function () {
+  $(this).closest('.skill-item').remove();
+});
+
+const getSkillEvaluation = function (skillRange) {
+  let skillEvaluation = '';
+  if (skillRange == 1) {
+    skillEvaluation = 'Beginner';
+  } else if (skillRange == 2) {
+    skillEvaluation = 'Moderate';
+  } else if (skillRange == 3) {
+    skillEvaluation = 'Good';
+  } else if (skillRange == 4) {
+    skillEvaluation = 'Very good';
+  } else if (skillRange == 5) {
+    skillEvaluation = 'Excellent';
+  }
+  return skillEvaluation;
+};
+
+$('.accordion-body').on('click', '.send-skill', function () {
+  const skillRange = $(this).closest('.skill-item').find('.form-range').val();
+  const skillEvaluation = getSkillEvaluation(skillRange);
+  const skill = $(this).closest('.skill-item').find('input[type=text]').val();
+  console.log(skill, skillEvaluation);
+});
+
+$('.skills').on('change', '.form-range', function () {
+  const skillRange = $(this).val();
+  let skillEvaluation = getSkillEvaluation(skillRange);
+  $(this).closest('.skill-range').siblings('.skill-evaluation').empty();
+  $(this).closest('.skill-range').siblings('.skill-evaluation').append(`
+    <span class=text-danger>${skillEvaluation}</span>
+  `);
+});
+
+//language
+$('.add-language').on('click', function () {
+  let languageID = $('.languages').find('.language-item:last-child').data('id');
+  if (!languageID) {
+    languageID = 0;
+  }
+  languageID++;
+  $(this).closest('div').siblings('.accordion-body').append(`
+    <div class='language-item mb-2' data-id=${languageID}>
+      <div class="mb-1">
+       <label for=language-${languageID} class=form-label>Language</label>
+       <input type="text" class=form-control id=language-${languageID} />
+       </div>
+       <div class="row">
+         <div class="language-range col-8">
+           <label for=language-range-${languageID} class=form-label>Level</label>
+           <input
+             type=range
+             class=form-range
+             min=0
+             max=5
+             value=0
+             step=1
+             id=language-range-${languageID}
+           />
+         </div>
+         <div class="language-evaluation col-4 pt-4"></div>
+       </div>
+      <button type=button class="btn btn-outline-danger remove-language">
+        <svg
+          xmlns=http://www.w3.org/2000/svg
+          width=16
+          height=16
+          fill=currentColor
+          class="bi bi-trash-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+          ></path>
+        </svg>
+        Remove Language
+      </button>
+      <button type="button" class="btn btn-outline-success send-language">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-check2"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
+          ></path>
+        </svg>
+        Done
+      </button>
+    </div>
+  `);
+});
+
+$('.accordion-body').on('click', '.remove-language', function () {
+  $(this).closest('.language-item').remove();
+});
+
+const getLanguageEvaluation = function (languageRange) {
+  let languageEvaluation = '';
+  if (languageRange == 1) {
+    languageEvaluation = 'Beginner';
+  } else if (languageRange == 2) {
+    languageEvaluation = 'Moderate';
+  } else if (languageRange == 3) {
+    languageEvaluation = 'Good';
+  } else if (languageRange == 4) {
+    languageEvaluation = 'Very good';
+  } else if (languageRange == 5) {
+    languageEvaluation = 'Fluent';
+  }
+  return languageEvaluation;
+};
+
+$('.accordion-body').on('click', '.send-language', function () {
+  const languageRange = $(this)
+    .closest('.language-item')
+    .find('.form-range')
+    .val();
+  const languageEvaluation = getLanguageEvaluation(languageRange);
+  const language = $(this)
+    .closest('.language-item')
+    .find('input[type=text]')
+    .val();
+  console.log(language, languageEvaluation);
+});
+
+$('.languages').on('change', '.form-range', function () {
+  const languageRange = $(this).val();
+  const languageEvaluation = getLanguageEvaluation(languageRange);
+  $(this).closest('.language-range').siblings('.language-evaluation').empty();
+  $(this).closest('.language-range').siblings('.language-evaluation').append(`
+    <span class=text-danger>${languageEvaluation}</span>
+  `);
+});
+
+//hobby
+$('.add-hobby').on('click', function () {
+  let hobbyID = $('.hobbies').find('.hobby-item:last-child').data('id');
+  if (!hobbyID) {
+    hobbyID = 0;
+  }
+  hobbyID++;
+  $(this).closest('div').siblings('.accordion-body').append(`
+    <div class='hobby-item mb-2' data-id=${hobbyID}>
+      <div class="mb-1">
+       <label for=hobby-${hobbyID} class=form-label>Hobby</label>
+       <input type="text" class=form-control id=hobby-${hobbyID} />
+       </div>
+      <button type=button class="btn btn-outline-danger remove-hobby">
+        <svg
+          xmlns=http://www.w3.org/2000/svg
+          width=16
+          height=16
+          fill=currentColor
+          class="bi bi-trash-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+          ></path>
+        </svg>
+        Remove Hobby
+      </button>
+      <button type="button" class="btn btn-outline-success send-hobby">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-check2"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
+          ></path>
+        </svg>
+          Done
+      </button>
+    </div>
+  `);
+});
+
+$('.accordion-body').on('click', '.remove-hobby', function () {
+  $(this).closest('.hobby-item').remove();
+});
+
+$('.accordion-body').on('click', '.send-hobby', function () {
+  console.log($(this).closest('.hobby-item').find('input').val());
+});
+
+//reference
+$('.add-reference').on('click', function () {
+  let referenceID = $('.references')
+    .find('.reference-item:last-child')
+    .data('id');
+  if (!referenceID) {
+    referenceID = 0;
+  }
+  referenceID++;
+  $(this).closest('div').siblings('.accordion-body').append(`
+    <div class='reference-item mb-2' data-id=${referenceID}>
+      <div class="mb-1">
+       <label for=reference-${referenceID} class=form-label>Reference</label>
+       <input type="text" class=form-control id=reference-${referenceID} />
+       <label for="referenceName-${referenceID}" class="form-label">Name</label>
+       <input type="text" class="form-control" id="referenceName-${referenceID}" />
+       </div>
+      <button type=button class="btn btn-outline-danger remove-reference">
+        <svg
+          xmlns=http://www.w3.org/2000/svg
+          width=16
+          height=16
+          fill=currentColor
+          class="bi bi-trash-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+          ></path>
+        </svg>
+        Remove Reference
+      </button>
+      <button type="button" class="btn btn-outline-success send-reference">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-check2"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
+          ></path>
+        </svg>
+          Done
+      </button>
+    </div>
+  `);
+});
+
+$('.accordion-body').on('click', '.remove-reference', function () {
+  $(this).closest('.reference-item').remove();
+});
+
+$('.accordion-body').on('click', '.send-reference', function () {
+  console.log(
+    $(this).closest('.reference-item').find('input:nth-child(2)').val()
+  );
+  console.log(
+    $(this).closest('.reference-item').find('input:nth-child(4)').val()
+  );
+});
--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 // const mongoose = require('mongoose')
-const bodyParser = require("body-parser");
-const api = require("./server/routes/api");
+const api = require('./server/routes/api');
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mydb', { useNewUrlParser: true })
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "dist")));
-app.use(express.static(path.join(__dirname, "node_modules")));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.use("/", api);
+app.use('/', api);
 
 const port = 3030;
 
 app.listen(port, function () {
-  console.log(`Running Running on port : ${port}`);
+  console.log(`Running server on port : ${port}`);
 });
